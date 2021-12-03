9711d43c923cc66e01a09e7d456c9d770419e72c
JeongKyunPark <park32323@gmail.com>
2021-12-03 14:30:50 +0900

    Create embedding.py
    embedding

---
+++ preprocess/embedding.py
@@ -0,0 +1,13 @@
+# Class for Embedding
+
+from gensim.models.word2vec import Word2Vec
+from gensim.models import FastText
+
+class embedding_Word2Vec():
+    '''
+    Input으로 token list를 받아서, 학습된 model을 출력하는 class 만들어보기~ class가 불필요하면 함수로 구현해도 될 듯.
+    '''
+    pass
+
+class embedding_FastText():
+    pass
