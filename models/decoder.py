203c0c9a6a45ca753714688298ede765051cf8c1
JeongKyunPark <park32323@gmail.com>
2021-12-07 23:46:53 +0900

    Update decoder.py
    decoder v1

--- models/decoder.py
+++ models/decoder.py
@@ -1,17 +1,20 @@
+# Decoder 담당 - 박희진 작품
+
 import torch
 import torch.nn as nn
 
 class DecoderLSTM(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers):
         super(DecoderLSTM, self).__init__()
-        self.embed = nn.Embedding(vocab_size, embed_size)
-        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
         self.linear = nn.Linear(hidden_size, vocab_size)
         self.dropout = nn.Dropout(0.5)
 
     def forward(self, features, captions):
+        print(features.size())
+        print(captions.size())
         embeddings = self.dropout(captions)
-        embeddings = torch.cat((features.unsqueeze(0), embeddings), dim=0)
+        embeddings = torch.cat((features.unsqueeze(1), embeddings), dim=1)
         hiddens, _ = self.lstm(embeddings)
         outputs = self.linear(hiddens)
         return outputs
