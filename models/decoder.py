0d3084acd65ac1b355461afdece783642c64e05c
sungjuoh <85203515+sungjuoh@users.noreply.github.com>
2021-12-29 22:19:42 +0900

    Update decoder.py

--- models/decoder.py
+++ models/decoder.py
@@ -179,6 +179,26 @@ class BahdanauAttention(nn.Module):
         attention_weight = attention_weight.squeeze(dim=2)
 
         return context, attention_weight
+    
+def visualize_attention(orig_image, words, atten_weights):
+    """Plots attention in the image sample.
+  <input>
+    - orig_image : image of original size
+    - words : list of tokens
+    - atten_weights : list of attention weights at each time step 
+    """
+    fig = plt.figure(figsize=(14,12)) 
+    len_tokens = len(words)
+    
+    for i in range(len(words)):
+        atten_current = atten_weights[i].detach().numpy()
+        atten_current = atten_current.reshape(7,7)       
+        ax = fig.add_subplot(len_tokens//2, len_tokens//2, i+1)
+        ax.set_title(words[i])
+        img = ax.imshow(np.squeeze(orig_image))
+        ax.imshow(atten_current, cmap='gray', alpha=0.8, extent=img.get_extent(), interpolation = 'bicubic')
+    plt.tight_layout()
+    plt.show()
 
 # class AttentionLSTM(nn.Module):
 #     def __init__(self, embedding_dim, hidden_dim, vocab_size, num_layers):
