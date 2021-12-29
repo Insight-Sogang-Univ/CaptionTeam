283cfb22f0ff7f464a36b2fcf2bd5f5a1e76db1c
JeongKyunPark <park32323@gmail.com>
2021-12-29 15:31:27 +0900

    preprocessing v.2
    add stopwords
    delete 홍길동 from start of the sentences

--- preprocess/preprocessing.py
+++ preprocess/preprocessing.py
@@ -22,11 +22,12 @@ if __name__=='__main__':
     
     print('Caption Preprocessing...')
     label_path = labeling.modify_label(RAW_LABEL, DATA_FOLDER, NAME_PATH, num=args.num)
+    labeling.stop_words(label_path, STOP_WORDS)
     print('Caption Preprocessing Complete')
     
-    print('Images to HDF5...')
-    image_process.jpg_to_hdf5(label_path, IMAGE_FILE, RAW_IMAGE, ERROR_LOGS)
-    print('HDF5 Created')
+#     print('Images to HDF5...')
+#     image_process.jpg_to_hdf5(label_path, IMAGE_FILE, RAW_IMAGE, ERROR_LOGS)
+#     print('HDF5 Created')
     
     print('Trimming Error Images...')
     labeling.clean_data(label_path, ERROR_LOGS)
