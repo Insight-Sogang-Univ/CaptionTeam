e7e1b5b1334617f4ce65aa74a3c06fd60d8625a0
Ga Young Ahn <89325388+Ga-Young-Ahn@users.noreply.github.com>
2021-12-29 23:59:19 +0900

    Update test.py

--- test.py
+++ test.py
@@ -12,26 +12,11 @@ import argparse, cv2
 from config.test_config import *
 from datasets.embedding import CaptionEmbedder
 from models.encoder_to_decoder import EncodertoDecoder
+import os
 
 def test(img_path, save_path=None, save=True, model='lstm'):
     
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    
-    image = read_image(img_path)
-    
-    _image = image.clone().to(device)
-    _image = image.type(torch.FloatTensor)
-    _image = _image.to(device)
-    
-    transform = transforms.Compose(
-        [
-        transforms.Resize((299,299)),
-        # transforms.ToTensor(),
-        transforms.Normalize((127.5, 127.5, 127.5), (255., 255., 255.))
-        ]
-    )
-
-    _image = transform(_image)
     
     #임베딩 불러오기
     embedder = CaptionEmbedder.load(EMBED_PATH)
@@ -44,34 +29,87 @@ def test(img_path, save_path=None, save=True, model='lstm'):
     model = model.to(device)
     model.eval()
     
-    #모델에 사진 넣기
-    caption = ''.join(model.caption_image(_image, embedder, max_length=FIXED_LENGTH))
-    print(caption)
-    
-    #사진에 캡션 추가
-    img = torch.permute(image, (1,2,0))
-    img = img.numpy()
-    
-    img = Image.fromarray(img)
-    
-    draw = ImageDraw.Draw(img) 
-    font=ImageFont.truetype("font/malgun.ttf",15)
-    
-    w, h = draw.textsize(caption, font=font)
-    img_W = img.size[0]
-    img_H = img.size[1]
-    
-    org=((img_W-w)/2, img_H-(img_H*0.1)) 
-    draw.text(org,caption,font=font,fill=(225,225,225))
+    if os.path.isdir(img_path):
+        
+        folder_img_path_list = []
+        possible_img_extension = ['.jpg', '.jpeg', '.JPG', '.bmp', '.png']
+        
+        for (root, dirs, files) in os.walk(img_path):
+            if files:
+                for file_name in files:
+                    if os.path.splitext(file_name)[1] in possible_img_extension:
+                        img_path = root + '/' + file_name
+                        
+                        # 경로에서 \를 모두 /로 바꿔줘야함
+                        img_path = img_path.replace('\\', '/') # \는 \\로 나타내야함         
+                        folder_img_path_list.append(img_path)
+                        
+        for f_img_path in folder_img_path_list:
+                #하나씩 이미지를 불러와서 캡션 달아주기
+            image = read_image(f_img_path)
+            
+            _image = image.clone()
+            _image = image.type(torch.FloatTensor)
+            _image = _image.to(device)
+            transform = transforms.Compose(
+                [
+                transforms.Resize((299,299)),
+                # transforms.ToTensor(),
+                transforms.Normalize((127.5, 127.5, 127.5), (255., 255., 255.))
+                ]
+            )
 
-    img = np.array(img)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    
-    if save:
-        cv2.imwrite(save_path, img)
+            _image = transform(_image)
+          
+            #모델에 사진 넣기
+            caption = ''.join(model.caption_image(_image, embedder, max_length=FIXED_LENGTH))
+            print(caption)    
+
     else:
-        cv2.imshow('imgwithcaption', img)
-        cv2.waitKey(0) 
+        image = read_image(img_path)
+        
+        _image = image.clone().to(device)
+        _image = image.type(torch.FloatTensor)
+        _image = _image.to(device)
+        
+        transform = transforms.Compose(
+            [
+            transforms.Resize((299,299)),
+            # transforms.ToTensor(),
+            transforms.Normalize((127.5, 127.5, 127.5), (255., 255., 255.))
+            ]
+        )
+
+        _image = transform(_image)
+        
+        #모델에 사진 넣기
+        caption = ''.join(model.caption_image(_image, embedder, max_length=FIXED_LENGTH))
+        print(caption)
+    
+        #사진에 캡션 추가
+        img = torch.permute(image, (1,2,0))
+        img = img.numpy()
+        
+        img = Image.fromarray(img)
+        
+        draw = ImageDraw.Draw(img) 
+        font=ImageFont.truetype("font/malgun.ttf",15)
+        
+        w, h = draw.textsize(caption, font=font)
+        img_W = img.size[0]
+        img_H = img.size[1]
+        
+        org=((img_W-w)/2, img_H-(img_H*0.1)) 
+        draw.text(org,caption,font=font,fill=(225,225,225))
+
+        img = np.array(img)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        
+        if save:
+            cv2.imwrite(save_path, img)
+        else:
+            cv2.imshow('imgwithcaption', img)
+            cv2.waitKey(0) 
 
 def get_args():
     parser = argparse.ArgumentParser(description = '각종 옵션')
